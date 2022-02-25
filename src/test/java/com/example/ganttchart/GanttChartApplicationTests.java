129c7d59ffcc0d87f5b5bb2b22817cefc84eb337
Чочевски Христијан <hristijancocevski@yahoo.com>
2022-02-25 08:49:21 +0100

    Project initialization

---
+++ src/test/java/com/example/ganttchart/GanttChartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ganttchart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GanttChartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
